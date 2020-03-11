7989a5a0ac1484f993dcc0986ee012089e28f09d
TomaszGladysz <tomecze@outlook.com>
2020-03-11 12:00:32 +0100

    last commit before merde chris

--- dev/js/main.js
+++ dev/js/main.js
@@ -17,3 +17,52 @@ sousMenu.addEventListener('click',function (){
 sousMenu.addEventListener('click',function (){
     monMenuHamburber.classList.toggle('visible');
 });
+
+
+// TESTETSTETSETESTETS
+// TESTETSTETSETESTETS
+// TESTETSTETSETESTETS
+
+for (var i = 1 ; i<10 ; i++ ){
+
+
+let targetURL = "URL API"; 
+let xhr = new XMLHttpRequest();
+
+xhr.addEventListener('readystatechange', function (event){
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        let json =JSON.parse(xhr.responseText);
+        // console.log(json.results[0].poster_path);
+
+
+            let task = document.querySelector('.wrapper');
+
+            let title = document.querySelector('h1');
+            let rating = document.querySelector('h2');
+            let poster = document.querySelector('img');
+            let imageWrap =document.querySelector('div');
+            let description = document.querySelector('p');
+            let filmWrap = document.querySelector('div');
+
+            filmWrap.className= 'wrapperMovie';
+            imageWrap.className= 'wrapperImage';
+            
+            wrapper.appendChild(filmWrap);
+            filmWrap.appendChild(title);
+            filmWrap.appendChild(imageWrap);
+            imageWrap.appendChild(poster);
+            imageWrap.appendChild(rating);
+            imageWrap.appendChild(description);
+            
+        }
+         
+
+    }
+})
+    xhr.open('GET', targetURL, true);
+    xhr.send();
+}
+  
+// TESTETSTETSETESTETS
+// TESTETSTETSETESTETS
+// TESTETSTETSETESTETS
